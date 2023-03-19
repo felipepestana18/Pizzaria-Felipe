811d64416e0a8dd9f7ee3271d347a210483826b5
felipepestanaa <felipe-pe27@hotmail.com>
2023-03-19 19:17:13 -0300

    add: Microsserviço Pedido, model

---
+++ Pizzaria.PedidoAPI/Model/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using Pizzaria.ClienteAPI.Model;
+using Pizzaria.PizzaAPI.Model;
+
+namespace Pizzaria.PedidoAPI.Model
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public  DateTime  DataPedido{ get; set; }
+
+        public int Status { get; set; }
+
+        public int QtdPedido { get; set; }
+
+        public decimal TotalPedido { get; set; }
+
+        public int QtdBebidas { get; set; }
+
+        public int QtdPizza { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public List<Pizza> Pizzas { get; set; }
+
+        public List<Bebida>  Bebidas { get; set; }
+
+
+
+    }
+}
---
+++ Pizzaria.PedidoAPI/Model/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace Pizzaria.PizzaAPI.Model
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
